02d4524c99696ab6d4f5bc98d18727cabd5875b7
Olaiwola Bolaji <olaiwola.bolaji@gmail.com>
2019-07-18 10:28:15 +0100

    modified

--- src/products/views.py
+++ src/products/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 #a place that handles my various webpages
 
-def home_view():
-        return HttpResponse('<h1> this is my turf, I am what the word says I am</h1>')
+def home_view(*args, **kwargs):
+        return HttpResponse("<h1> this is my turf, I am what the word says I am</h1>")
